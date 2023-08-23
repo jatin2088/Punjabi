444c4004c6ff7af6fa96252b9e6e5bb45ddb5a82
jatin2088 <40684583+jatin2088@users.noreply.github.com>
2023-08-23 14:38:06 +0530

    app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/",methods=['GET'])
-def hello():
-    return "Hello World"
 
 @app.route('/upload', methods=['POST'])
 def upload():
