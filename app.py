449ef76d2167771a8dd1ba4e0b88e903effdb1cd
jatin2088 <40684583+jatin2088@users.noreply.github.com>
2023-08-23 15:20:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,74 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import cv2
 import numpy as np
-import base64
-import io
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/process-image', methods=['POST'])
-def process_image():
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World"
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the image file and image name from the request
+    image_file = request.files['image']
+    image_name = request.form['imageName']
+
+    # Convert the uploaded image to a NumPy array
+    image_data = image_file.read()
+    nparr = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # Image processing
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(gray, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    ret, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    height, width = img.shape
+
+    # Open the annotations file with the same name as the image (without its extension)
+    image_name = image_name.split('.')[0]
     try:
-        data = request.json
+        with open(f"annotations/{image_name}.txt", 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        return jsonify({"error": "Not a Valid Image", "text": None})
 
-        # Decode base64 image
-        image_data = base64.b64decode(data["image"].split(",")[-1])
-        image = cv2.imdecode(np.frombuffer(image_data, np.uint8), -1)
+    text = ''
+    prev_y_center = 0
+    prev_x_center = 0
+    prev_w = 0
+    for line in lines:
+        line = line.strip()
 
-        # Image processing
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        kernel = np.ones((1, 1), np.uint8)
-        img = cv2.dilate(gray, kernel, iterations=1)
-        img = cv2.erode(img, kernel, iterations=1)
-        img = cv2.GaussianBlur(img, (5, 5), 0)
-        ret, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # If the line is empty, it indicates a space
+        if not line:
+            text += ' '
+            continue
 
-        height, width = img.shape
+        line = line.split()
+        char, x_center, y_center, w, h = line[0], float(line[1]), float(line[2]), float(line[3]), float(line[4])
 
-        annotation = data.get("annotation", "").splitlines()
+        # Denormalize the coordinates
+        x_center, y_center, w, h = x_center * width, y_center * height, w * width, h * height
 
-        text = ''
-        prev_y_center = 0
-        prev_x_center = 0
-        prev_w = 0
+        x, y = int(x_center - w // 2), int(y_center - h // 2)
+        w, h = int(w), int(h)
 
-        for line in annotation:
-            line = line.split()
-            char, x_center, y_center, w, h = line[0], float(line[1]), float(line[2]), float(line[3]), float(line[4])
-            x_center, y_center, w, h = x_center * width, y_center * height, w * width, h * height
-            x, y = int(x_center - w//2), int(y_center - h//2)
-            w, h = int(w), int(h)
+        if y_center - prev_y_center > h:
+            text += '\n'
 
-            if y_center - prev_y_center > h:
-                text += '\n'
-            else:
-                prev_right_edge = prev_x_center + prev_w / 2
-                curr_left_edge = x_center - w / 2
-                if prev_right_edge < curr_left_edge:
-                    text += ' '
+        prev_y_center = y_center
+        prev_x_center = x_center
+        prev_w = w
 
-            prev_y_center = y_center
-            prev_x_center = x_center
-            prev_w = w
-            text += char
+        text += char
 
-        return jsonify({'result': text})
-
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    return jsonify({"error": None, "text": text})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)  # Adjust port if necessary
+    app.run(port=8080, debug=True, host="0.0.0.0", threaded=True, use_reloader=True, passthrough_errors=True)
