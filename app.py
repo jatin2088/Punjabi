0f523fa41c4aedc79fe410ff056c585aacf2b134
Jaspreet <40684583+jatin2088@users.noreply.github.com>
2023-09-02 12:02:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 import requests
 from flask_cors import CORS
+from PIL import Image
+from PIL import PngImagePlugin
+from io import BytesIO
 import os
 import json
 
@@ -10,7 +13,6 @@ app = Flask(__name__)
 CORS(app)
 
 GITHUB_REPO_URL = "https://raw.githubusercontent.com/jatin2088/Punjabi/main/annotations/"
-
 OCR_SPACE_API_KEY = 'K81439199088957'  # replace with your OCR.space API key
 
 @app.route("/", methods=['GET'])
@@ -20,19 +22,28 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     image_file = request.files['image']
-    image_ext = os.path.splitext(image_file.filename)[-1].lower()
+    file_extension = image_file.filename.split('.')[-1].lower()
 
-    if image_ext != '.png':
+    if file_extension != 'png':
         return render_template("index.html", text="Check file format.")
-    
-    image_data = image_file.read()
-    nparr = np.frombuffer(image_data, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    image_name, _ = os.path.splitext(image_file.filename)
-    response = requests.get(GITHUB_REPO_URL + image_name + '.txt')
+    image_data = image_file.read()
+    
+    # Read the uploaded image to PIL Image to fetch metadata
+    pil_img = Image.open(BytesIO(image_data))
+    meta = pil_img.info
+    unique_id = meta.get("uniqueID", "")
+
+    # Convert PIL image to OpenCV format
+    nparr = np.array(pil_img)
+    image = cv2.cvtColor(nparr, cv2.COLOR_RGB2BGR)
+
+    height, width, _ = image.shape
+
+    response = requests.get(GITHUB_REPO_URL + unique_id + '.txt')
 
     if response.status_code == 200:
+        # Normal flow for images with annotations
         lines = response.text.splitlines()
         text = ''
         prev_y_center = 0
@@ -47,25 +58,23 @@ def upload():
 
             if y_center - prev_y_center > h:
                 text += '\n'
+
             prev_y_center = y_center
             text += char
-        return render_template("index.html", text=text)
     else:
-        # Use OCR.space API to extract text
+        # Use OCR.space API to extract text if annotation not available
         url = "https://api.ocr.space/parse/image"
         headers = {"apikey": OCR_SPACE_API_KEY}
         files = {"file": ("image.png", image_data, 'image/png')}
         ocr_response = requests.post(url, headers=headers, files=files)
         ocr_result = json.loads(ocr_response.text)
         
-        print(ocr_result)  # Debugging
-        
         if ocr_result.get('IsErroredOnProcessing') == False and 'ParsedResults' in ocr_result:
             text = ocr_result['ParsedResults'][0]['ParsedText']
-            return render_template("index.html", text=text)
         else:
-            print(f"Error Message: {ocr_result.get('ErrorMessage', 'Unknown Error')}")
-            return render_template("index.html", text="")
+            text = ""
+
+    return render_template("index.html", text=text)
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True, host="0.0.0.0", threaded=True, use_reloader=True, passthrough_errors=True)
