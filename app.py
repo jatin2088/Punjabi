e0c5ff1bf1f1a6a8007230db8de11465d7770ae5
Jaspreet <40684583+jatin2088@users.noreply.github.com>
2023-09-02 10:50:02 +0530

    Update and rename appp.py to app.py

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 import cv2
 import numpy as np
+import requests
 from flask_cors import CORS
+import os
+from PIL import Image
+import pytesseract
 
 app = Flask(__name__)
 CORS(app)
 
+GITHUB_REPO_URL = "https://raw.githubusercontent.com/jatin2088/Punjabi/main/annotations/"
+
 @app.route("/", methods=['GET'])
-def hello():
-    return "Hello World"
+def index():
+    return render_template("index.html")
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Get the image file and image name from the request
     image_file = request.files['image']
-    image_name = request.form['imageName']
+    image_ext = os.path.splitext(image_file.filename)[1].lower()
 
-    # Convert the uploaded image to a NumPy array
+    if image_ext != ".png":
+        return render_template("index.html", text="Check file format.")
+
     image_data = image_file.read()
     nparr = np.frombuffer(image_data, np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # Image processing
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     kernel = np.ones((1, 1), np.uint8)
     img = cv2.dilate(gray, kernel, iterations=1)
@@ -30,45 +36,34 @@ def upload():
     ret, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     height, width = img.shape
 
-    # Open the annotations file with the same name as the image (without its extension)
-    image_name = image_name.split('.')[0]
-    try:
-        with open(f"annotations/{image_name}.txt", 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-    except FileNotFoundError:
-        return jsonify({"error": "Not a Valid Image", "text": None})
+    image_name, _ = os.path.splitext(image_file.filename)
+    response = requests.get(GITHUB_REPO_URL + image_name + '.txt')
 
+    if response.status_code != 200:
+        # Fallback to OCR if annotation not found
+        text = pytesseract.image_to_string(Image.fromarray(img), lang='pan')
+        return render_template("index.html", text=text if text else "")
+
+    lines = response.text.splitlines()
     text = ''
     prev_y_center = 0
-    prev_x_center = 0
-    prev_w = 0
     for line in lines:
         line = line.strip()
-
-        # If the line is empty, it indicates a space
         if not line:
             text += ' '
             continue
 
-        line = line.split()
-        char, x_center, y_center, w, h = line[0], float(line[1]), float(line[2]), float(line[3]), float(line[4])
-
-        # Denormalize the coordinates
+        line_data = line.split()
+        char, x_center, y_center, w, h = line_data[0], float(line_data[1]), float(line_data[2]), float(line_data[3]), float(line_data[4])
         x_center, y_center, w, h = x_center * width, y_center * height, w * width, h * height
-
-        x, y = int(x_center - w // 2), int(y_center - h // 2)
-        w, h = int(w), int(h)
-
+        
         if y_center - prev_y_center > h:
             text += '\n'
-
         prev_y_center = y_center
-        prev_x_center = x_center
-        prev_w = w
-
+        
         text += char
 
-    return jsonify({"error": None, "text": text})
+    return render_template("index.html", text=text)
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True, host="0.0.0.0", threaded=True, use_reloader=True, passthrough_errors=True)
