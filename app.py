fe1b645a0aa3862bad0a1ba7b7fef37265121bf8
Jaspreet <40684583+jatin2088@users.noreply.github.com>
2023-09-05 16:47:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,66 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask, request, render_template
 import cv2
+import numpy as np
 import requests
-import pytesseract
-import re
+from flask_cors import CORS
 from PIL import Image
 from io import BytesIO
-import numpy as np
-from flask_cors import CORS
+import pytesseract
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Download Tesseract Data
-TESSDATA_PREFIX = '/tmp/tessdata'
-TESSDATA_URL = 'https://github.com/jatin2088/Punjabi/main/tesseract-ocr/4.00/tessdata/'
+# Update the TESSDATA_PREFIX as per your setup
+os.environ['TESSDATA_PREFIX'] = '/tmp/tessdata'
 
-if not os.path.exists(TESSDATA_PREFIX):
-    os.makedirs(TESSDATA_PREFIX)
-
-required_files = ['pan.traineddata']
-
-for file in required_files:
-    response = requests.get(TESSDATA_URL + file)
-    if response.status_code == 200:
-        with open(os.path.join(TESSDATA_PREFIX, file), 'wb') as f:
-            f.write(response.content)
-    else:
-        print(f"Failed to download {file}")
-
-os.environ['TESSDATA_PREFIX'] = TESSDATA_PREFIX
-
-# GitHub Repo URL
 GITHUB_REPO_URL = "https://raw.githubusercontent.com/jatin2088/Punjabi/main/annotations/"
-
-# Filter Punjabi Text
-def filter_punjabi(text):
-    pattern = re.compile('[^\u0A00-\u0A7F  \n]')
-    return pattern.sub('', text)
 
 @app.route("/", methods=['GET'])
 def index():
     return render_template("index.html")
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
+def upload():
     image_file = request.files['image']
     file_extension = image_file.filename.split('.')[-1].lower()
 
-    if file_extension not in ('png', 'jpg', 'jpeg'):
+    if file_extension != 'png':
         return render_template("index.html", text="Check file format.")
 
     pil_img = Image.open(BytesIO(image_file.read()))
     meta = pil_img.info
     unique_id = meta.get("uniqueID", "")
 
-    nparr = np.array(pil_img)
-    image = cv2.cvtColor(nparr, cv2.COLOR_RGB2BGR)
-
+    # Check for annotations first
     response = requests.get(GITHUB_REPO_URL + unique_id + '.txt')
-    
     if response.status_code == 200:
         lines = response.text.splitlines()
-        text = " ".join(lines)
+        text = ''
+        prev_y_center = 0
+
+        for line in lines:
+            line = line.strip()
+            if not line:
+                text += ' '
+                continue
+
+            line_data = line.split()
+            text += line_data[0]
+        
         return render_template("index.html", text=text)
+
+    # Fallback to OCR if annotations not found
     else:
-        try:
-            extracted_text = pytesseract.image_to_string(Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)), lang='pan', config='--psm 6')
-            filtered_text = filter_punjabi(extracted_text)
-            return render_template("index.html", text=filtered_text)
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return render_template("index.html", text="Error in processing image.")
+        nparr = np.array(pil_img)
+        image = cv2.cvtColor(nparr, cv2.COLOR_RGB2BGR)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        
+        # Extract text using Tesseract
+        text = pytesseract.image_to_string(img, lang='pan', config='--psm 6')
+        
+        return render_template("index.html", text=text)
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True, host='0.0.0.0')
+    app.run(port=8080, debug=True, host="0.0.0.0", threaded=True, use_reloader=True, passthrough_errors=True)
