6c70c8c22746ddda0983c831bc296ecb797f0443
Jaspreet <40684583+jatin2088@users.noreply.github.com>
2023-09-05 15:39:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,43 @@
 from flask import Flask, request, render_template
+import os
 import cv2
 import numpy as np
-import requests
-from flask_cors import CORS
-from PIL import Image
-from io import BytesIO
-import os
 import pytesseract
+from PIL import Image
+import requests
+from io import BytesIO
 import re
+
+# Initialize Flask
+app = Flask(__name__)
 
 # Function to download Tesseract data
 def download_tessdata_from_github():
     tessdata_url = "https://github.com/jatin2088/Punjabi/raw/main/tesseract-ocr/4.00/tessdata/"
     tessdata_files = ['pan.traineddata']
-
     tessdata_dir = './tessdata/'
     os.makedirs(tessdata_dir, exist_ok=True)
-
     for file in tessdata_files:
         response = requests.get(f"{tessdata_url}{file}", allow_redirects=True)
         with open(os.path.join(tessdata_dir, file), 'wb') as f:
             f.write(response.content)
 
+# Set TESSDATA_PREFIX environment variable
+os.environ['TESSDATA_PREFIX'] = './tessdata/'
+
 # Download Tesseract data
 download_tessdata_from_github()
 
-# Set TESSDATA_PREFIX
-os.environ['TESSDATA_PREFIX'] = './tessdata/'
+# Function to preprocess image
+def preprocess_image(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.GaussianBlur(gray, (5, 5), 0)
+    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return img
 
-# Initialize Flask app
-app = Flask(__name__)
-CORS(app)
-
-# GitHub repository URL for annotations
-GITHUB_REPO_URL = "https://raw.githubusercontent.com/jatin2088/Punjabi/main/annotations/"
-
-# Perform OCR using Tesseract
+# Function to perform OCR
 def perform_ocr(img):
+    img = Image.fromarray(img)
     text = pytesseract.image_to_string(img, lang='pan', config='--psm 6')
     return text
 
@@ -47,31 +48,23 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     image_file = request.files['image']
-    file_extension = image_file.filename.split('.')[-1].lower()
-
-    if file_extension != 'png':
-        return render_template("index.html", text="Check file format.")
-
     pil_img = Image.open(BytesIO(image_file.read()))
     nparr = np.array(pil_img)
-    img = cv2.cvtColor(nparr, cv2.COLOR_RGB2BGR)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    image = cv2.cvtColor(nparr, cv2.COLOR_RGB2BGR)
     unique_id = pil_img.info.get("uniqueID", "")
-
-    # Attempt to fetch annotations first
+    GITHUB_REPO_URL = "https://raw.githubusercontent.com/jatin2088/Punjabi/main/annotations/"
     response = requests.get(GITHUB_REPO_URL + unique_id + '.txt')
+
     if response.status_code == 200:
         lines = response.text.splitlines()
-        text = ''
-        prev_y_center = 0
+        text = ""
         for line in lines:
-            # ... Your existing logic for using annotations
+            text += line + "\n"
         return render_template("index.html", text=text)
     else:
-        # Fallback to Tesseract OCR
-        extracted_text = perform_ocr(gray)
-        return render_template("index.html", text=extracted_text)
+        processed_image = preprocess_image(image)
+        ocr_text = perform_ocr(processed_image)
+        return render_template("index.html", text=ocr_text)
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True, host="0.0.0.0", threaded=True, use_reloader=True, passthrough_errors=True)
+    app.run(port=8080, debug=True, host="0.0.0.0")
