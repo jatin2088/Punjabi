6d454e636ab7f875ffc85a3148f4b7080ac2dfb4
Jaspreet <40684583+jatin2088@users.noreply.github.com>
2023-09-08 15:52:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
 from flask import Flask, request, render_template
-import os
 import cv2
 import numpy as np
 import requests
 from flask_cors import CORS
 from PIL import Image
 from io import BytesIO
-import re
-import urllib.request
+import os
 import pytesseract
+import re
 
-# Create a directory to hold the pan.traineddata file
-os.makedirs("tessdata", exist_ok=True)
-TESSDATA_PATH = './tessdata'
-
-# Download pan.traineddata from GitHub repo to TESSDATA_PATH
-PAN_TRAINEDDATA_URL = 'https://github.com/jatin2088/Punjabi/raw/main/tesseract-ocr/4.00/tessdata/pan.traineddata'
-urllib.request.urlretrieve(PAN_TRAINEDDATA_URL, f"{TESSDATA_PATH}/pan.traineddata")
-
-# Set TESSDATA_PREFIX environment variable
-os.environ['TESSDATA_PREFIX'] = TESSDATA_PATH + '/'
-
+# Initialize Flask
 app = Flask(__name__)
 CORS(app)
+
+# GitHub Repo URL
 GITHUB_REPO_URL = "https://raw.githubusercontent.com/jatin2088/Punjabi/main/annotations/"
 
+# Filter Punjabi Text
 def filter_punjabi(text):
     pattern = re.compile('[^\u0A00-\u0A7F  \n]')
     return pattern.sub('', text)
@@ -45,43 +37,46 @@ def upload():
     meta = pil_img.info
     unique_id = meta.get("uniqueID", "")
 
-    response = requests.get(GITHUB_REPO_URL + unique_id + '.txt')
-    
-    if response.status_code != 200:
-        nparr = np.array(pil_img.convert('RGB'))
-        gray = cv2.cvtColor(nparr, cv2.COLOR_BGR2GRAY)
-        gray_pil = Image.fromarray(gray)
-        text = pytesseract.image_to_string(gray_pil, lang='pan', config='--psm 6')
-        filtered_text = filter_punjabi(text)
-        return render_template("index.html", text=filtered_text)
-    
     nparr = np.array(pil_img)
     image = cv2.cvtColor(nparr, cv2.COLOR_RGB2BGR)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    response = requests.get(GITHUB_REPO_URL + unique_id + '.txt')
     
-    height, width = gray.shape
-    lines = response.text.splitlines()
-    text = ''
-    prev_y_center = 0
+    if response.status_code == 200:
+        # Existing logic
+        lines = response.text.splitlines()
+        text = ''
+        for line in lines:
+            # Your logic here
+            pass
+        return render_template("index.html", text=text)
+    else:
+        try:
+            # Download the Tesseract trained data file from GitHub
+            tesseract_data_url = 'https://github.com/jatin2088/Punjabi/raw/main/tesseract-ocr/4.00/tessdata/pan.traineddata'
+            tesseract_data = requests.get(tesseract_data_url).content
 
-    for line in lines:
-        line = line.strip()
-        if not line:
-            text += ' '
-            continue
+            # Store the trained data in a temporary directory
+            TESSDATA_PREFIX = '/tmp/tessdata'
+            os.makedirs(TESSDATA_PREFIX, exist_ok=True)
+            with open(os.path.join(TESSDATA_PREFIX, 'pan.traineddata'), 'wb') as f:
+                f.write(tesseract_data)
 
-        line_data = line.split()
-        char, x_center, y_center, w, h = line_data[0], float(line_data[1]), float(line_data[2]), float(line_data[3]), float(line_data[4])
-        
-        x_center, y_center, w, h = x_center * width, y_center * height, w * width, h * height
+            # Set the TESSDATA_PREFIX environment variable
+            os.environ['TESSDATA_PREFIX'] = TESSDATA_PREFIX
 
-        if y_center - prev_y_center > h:
-            text += '\n'
-
-        prev_y_center = y_center
-        text += char
-
-    return render_template("index.html", text=text)
+            # Perform OCR
+            extracted_text = pytesseract.image_to_string(
+                Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)),
+                lang='pan',
+                config='--psm 6'
+            )
+            
+            filtered_text = filter_punjabi(extracted_text)
+            return render_template("index.html", text=filtered_text)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return render_template("index.html", text="Error in processing image.")
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True, host="0.0.0.0", threaded=True, use_reloader=True, passthrough_errors=True)
